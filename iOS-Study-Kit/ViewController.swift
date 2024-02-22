e45067d32eb492c9172d342c3f487aa6546d5026
Anselmus Pavel Adriska <anselmus.pavel@inglab.sg>
2024-02-22 21:55:49 +0700

    Project starter

---
+++ iOS-Study-Kit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Study-Kit
+//
+//  Created by Anselmus Pavel Adriska on 22/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
